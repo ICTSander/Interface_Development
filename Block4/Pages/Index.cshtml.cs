f5de529990a7e17206c9eaaabfef447cfb06a70a
LAPTOP003 <xkeleter3@gmail.com>
2026-05-18 11:09:25 +0200

    feat(cart): added back-end to manage cart items added database from reference project de-commented scope Repository inside project start-up file

--- Block4/Pages/Index.cshtml.cs
+++ Block4/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
 using Block4.Models;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json;
@@ -9,39 +11,60 @@ namespace Block4.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        // Data Access layer
+        private readonly IProductRepository _productRepository;
+
+        
+
 
 
         // cart item
+
+        public required IList<Product> products;
+
         public required List<Cart> CartItems;
-        public required float cartShippingCost = getShippingCost();
-        public required float cartSubtotal = getCartSubtotal();
+        public required decimal cartShippingCost = getShippingCost();
+        public required decimal cartSubtotal;
         public required string removeImg = setCartItemRemmoveImagePath();
-        private static float getShippingCost()
+        private static decimal getShippingCost()
         {
             // default is 0;
             decimal shippingCost = 0;
-            throw new NotImplementedException();
+            return shippingCost;
         }
-        private static float getCartSubtotal()
+        private decimal getCartSubtotal()
         {
-            throw new NotImplementedException();
+            decimal cartSubtotal = 0;
+            foreach (var cart in CartItems)
+            {
+                int quatity = cart.Quantity;
+                decimal price = cart.Product.Price;
+                cartSubtotal += price * quatity;
+            }
+
+            return cartSubtotal;
         }
         private static string setCartItemRemmoveImagePath()
         {
-            throw new NotImplementedException();
+            // TODO: edit the path to the image when image exist
+            string removeImg = "./notFound";
+            return removeImg;
         }
 
 
-
+        public IndexModel(ILogger<IndexModel> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            products = new List<Product>();
+            _productRepository = productRepository;
+        }
 
         public void OnGet()
         {
+            products = _productRepository.GetAllProducts().ToList();
             CartItems.Clear();
             LoadCart();
+            cartSubtotal = getCartSubtotal();
         }
 
         private void LoadCart()
