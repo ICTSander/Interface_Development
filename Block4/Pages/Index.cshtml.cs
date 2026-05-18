b9b00c4b28f66f29b1a13a6699d0d24f70cfac6f
Torchbread <2506519lomas@zuyd.nl>
2026-05-18 13:44:10 +0200

    Added Productlist

--- Block4/Pages/Index.cshtml.cs
+++ Block4/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +8,20 @@ namespace Block4.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IProductRepository _productRepository;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Product> Products;
+
+        public IndexModel(ILogger<IndexModel> logger, IProductRepository productRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
+            Products = new List<Product>();
         }
 
         public void OnGet()
         {
-
+            Products = _productRepository.GetAllProducts().ToList();
         }
     }
 }
